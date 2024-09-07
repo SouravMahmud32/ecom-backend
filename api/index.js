47600eea4f0ea7e3c8d26f6424ac5aa7d5c47803
sourav-mahmud <souravmahmud32@gmail.com>
2024-09-07 20:15:23 +0600

    updated

--- api/index.js
+++ api/index.js
@@ -13,6 +13,17 @@ connectDB();
 
 const app = express();
 
+const path = require('path');
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
 // Middleware for CORS and JSON parsing
 app.use(cors());
 app.use(express.json());
